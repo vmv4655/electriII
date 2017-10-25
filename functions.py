8f311f44087f7201279b9c50db1a5af279ea4784
vmora <vmv4655@gmail.com>
2017-10-25 02:38:09 -0600

    Me falto agregar el archivo functions.py al commit

--- functions.py
+++ functions.py
@@ -53,31 +53,49 @@ def getRecFromPol(magnitud, angulo):
 	else:
 		return False
 
+#Clase padre para modelar tanto la fuente delta como la estrella
+#se utiliza herencia porque ambas fuentes van a tener los mismos
+#metodos
+class Fuente(object):
+	Van = None
+	Vbn = None
+	Vcn = None
+	Vab = None
+	Vbc	= None
+	Vca	= None
 
+	#Devuelve el valor de una fase a partir de otra
+	def calcularAnguloVoltajes(self, v, operacion, valor):
+		angulo = getPolFromRec(v)
+		angulo = angulo[1]
+		magnitud = getPolFromRec(v)
+		magnitud = magnitud[0]
+		if(operacion == "+"):
+			angulo += valor
+		elif(operacion == "-"):
+			angulo -= valor
+		return getRecFromPol(magnitud, angulo)
 
+	#Devuelve un un voltaje de fase a partir de un voltaje de linea
+	def calcularVFases(self, v, secuencia):
+		if(secuencia == "ABC"):
+			cte = getRecFromPol(math.sqrt(3), 30)
+		elif(secuencia == "ACB"):
+			cte = getRecFromPol(math.sqrt(3), -30)
+		res = v/cte
+		return res
 
-#Determina voltajes de fase a partir de voltajes
-#de linea para una conexion de fuentes en delta.
-##Entradas
-#voltaje: voltaje entre dos puntos Vab, Vbc, Vca... en formato rectangular
-#secuencia 
-"""def getVFaseDelta(voltaje, secuencia, vin, vout):
-	if(isRecComplex(voltaje)):
-		value = None
-		if(secuencia == "+"):
-			value = getRecFromPol(math.sqrt(3), 30)
-		elif(sencuencia == "-"):
-			value = getRecFromPol(math.sqrt(3), -30)
+	#Devuelve un voltaje de linea a partir de un voltaje de fase
+	def calcularVLinea(self, v, secuencia):
+		if(secuencia == "ABC"):
+			cte = getRecFromPol(math.sqrt(3), 30)
+		elif(secuencia == "ACB"):
+			cte = getRecFromPol(math.sqrt(3), -30)
+		res = v*cte
+		return res
 
-		if(vin == "AB"):
-			
-		elif(vin == "BC"):
-
-		elif(vin == "AC"):"""
-
-
-class FuenteDelta(object):
-	def __init__(self, Van = None , Vbn = None, Vcn = None, Vab = None, Vbc = None, Vca = None, secuencia = "ABC"):
+	#Calcula todos los voltajes de fase y de linea a partir de uno dado
+	def calcularVoltajes(self, Van = None , Vbn = None, Vcn = None, Vab = None, Vbc = None, Vca = None, secuencia = "ABC"):
 		self.Van = Van
 		self.Vbn = Vbn
 		self.Vcn = Vcn
@@ -86,24 +104,6 @@ class FuenteDelta(object):
 		self.Vca = Vca
 		self.secuencia = secuencia
 
-		def calcularAnguloVoltajes(v, operacion, valor):
-			angulo = getPolFromRec(v)
-			angulo = angulo[1]
-			magnitud = getPolFromRec(v)
-			magnitud = magnitud[0]
-			if(operacion == "+"):
-				angulo += valor
-			elif(operacion == "-"):
-				angulo -= valor
-			return getRecFromPol(magnitud, angulo)
-
-		def calcularVFases(v, secuencia):
-			if(secuencia == "ABC"):
-				cte = getRecFromPol(math.sqrt(3), 30)
-			elif(secuencia == "ACB"):
-				cte = getRecFromPol(math.sqrt(3), -30)
-			res = v/cte
-			return res
 		#Calculando voltajes a partir de datos iniciales
 		#Si tenemos Vab
 		if(self.Vab != None):
@@ -112,20 +112,20 @@ class FuenteDelta(object):
 				#Calculando voltajes de linea
 				if(self.Vbc == None):
 					#Vbc = #mismo voltaje angulo menos 120
-					self.Vbc = calcularAnguloVoltajes(self.Vab, "-", 120)
+					self.Vbc = self.calcularAnguloVoltajes(self.Vab, "-", 120)
 				if(self.Vca == None):
 					#Vca = #mismo voltaje angulo mas 120
-					self.Vca = calcularAnguloVoltajes(self.Vab, "+", 120)
+					self.Vca = self.calcularAnguloVoltajes(self.Vab, "+", 120)
 
 			#Secuencia negativa
 			if(self.secuencia == "ACB"):
 				#Calculando voltajes de linea
 				if(self.Vbc == None):
 					#Vbc = #mismo voltaje angulo mas 120
-					self.Vbc = calcularAnguloVoltajes(self.Vab, "+", 120)
+					self.Vbc = self.calcularAnguloVoltajes(self.Vab, "+", 120)
 				if(self.Vca == None):
 					#Vca = #mismo voltaje angulo menos 120
-					self.Vca = calcularAnguloVoltajes(self.Vab, "-", 120)
+					self.Vca = self.calcularAnguloVoltajes(self.Vab, "-", 120)
 		
 
 		#Si tenemos Vbc
@@ -135,20 +135,20 @@ class FuenteDelta(object):
 				#Calculando voltajes de linea
 				if(self.Vab == None):
 					#Vab = #mismo voltaje angulo mas 120
-					self.Vab = calcularAnguloVoltajes(self.Vbc, "+", 120)
+					self.Vab = self.calcularAnguloVoltajes(self.Vbc, "+", 120)
 				if(self.Vca == None):
 					#Vca = #mismo voltaje angulo mas 240
-					self.Vca = calcularAnguloVoltajes(self.Vbc, "+", 240)
+					self.Vca = self.calcularAnguloVoltajes(self.Vbc, "+", 240)
 
 			#Secuencia negativa
 			if(self.secuencia == "ACB"):
 				#Calculando voltajes de linea
 				if(self.Vab == None):
 					#Vab = #mismo voltaje angulo menos 120
-					self.Vab = calcularAnguloVoltajes(self.Vbc, "-", 120)
+					self.Vab = self.calcularAnguloVoltajes(self.Vbc, "-", 120)
 				if(self.Vca == None):
 					#Vca = #mismo voltaje angulo menos 240
-					self.Vca = calcularAnguloVoltajes(self.Vbc, "-", 240)
+					self.Vca = self.calcularAnguloVoltajes(self.Vbc, "-", 240)
 
 
 		#Si tenemos Vca
@@ -158,44 +158,136 @@ class FuenteDelta(object):
 				#Calculando voltajes de linea
 				if(self.Vab == None):
 					#Vab = #mismo voltaje angulo menos 120
-					self.Vab = calcularAnguloVoltajes(self.Vca, "-", 120)
+					self.Vab = self.calcularAnguloVoltajes(self.Vca, "-", 120)
 				if(self.Vbc == None):
 					#Vbc = #mismo voltaje angulo menos 240
-					self.Vbc = calcularAnguloVoltajes(self.Vca, "-", 240)
+					self.Vbc = self.calcularAnguloVoltajes(self.Vca, "-", 240)
 
 			#Secuencia negativa
 			if(self.secuencia == "ACB"):
 				#Calculando voltajes de linea
 				if(self.Vab == None):
 					#Vab = #mismo voltaje angulo mas 120
-					self.Vab = calcularAnguloVoltajes(self.Vca, "+", 120)
+					self.Vab = self.calcularAnguloVoltajes(self.Vca, "+", 120)
 				if(self.Vbc == None):
 					#Vbc = #mismo voltaje angulo mas 240
-					self.Vbc = calcularAnguloVoltajes(self.Vca, "+", 240)
+					self.Vbc = self.calcularAnguloVoltajes(self.Vca, "+", 240)
+		#Si tenemos algun voltaje de lineas, calculamos los de fase
+		if(self.Vab != None or self.Vbc != None or self.Vca != None):
+			#Calculando voltajes de fase
+			if(self.Van == None):
+				#Van = Vab/sqrt(3)<30 
+				self.Van = self.calcularVFases(self.Vab, self.secuencia)
+			if(self.Vbn == None):
+				#Vbn = Vbc/sqrt(3)<30
+				self.Vbn = self.calcularVFases(self.Vbc, self.secuencia)
+			if(self.Vcn == None):
+				#Vcn = Vca/sqrt(3)<30
+				self.Vcn = self.calcularVFases(self.Vca, self.secuencia)
 
-		#Calculando voltajes de fase
-		if(self.Van == None):
-			#Van = Vab/sqrt(3)<30 
-			self.Van = calcularVFases(self.Vab, self.secuencia)
-		if(self.Vbn == None):
-			#Vbn = Vbc/sqrt(3)<30
-			self.Vbn = calcularVFases(self.Vbc, self.secuencia)
-		if(self.Vcn == None):
-			#Vcn = Vca/sqrt(3)<30
-			self.Vcn = calcularVFases(self.Vca, self.secuencia)
 
+		#Si nos dan un voltaje de fase, calcular todos los otros ademas de los de linea
+		if(self.Van != None):
+			if(self.secuencia == "ABC"):
+				if(self.Vbn == None):
+					self.Vbn = self.calcularAnguloVoltajes(self.Van, "-", 120)
+
+				if(self.Vcn == None):
+					self.Vcn = self.calcularAnguloVoltajes(self.Van, "+", 120)
+			
+			if(self.secuencia == "ACB"):
+				if(self.Vbn == None):
+					self.Vbn = self.calcularAnguloVoltajes(self.Van, "+", 120)
+				if(self.Vcn == None):
+					self.Vcn = self.calcularAnguloVoltajes(self.Van, "-", 120)
+
+
+		if(self.Vbn != None):
+			if(self.secuencia == "ABC"):
+				if(self.Van == None):
+					self.Van = self.calcularAnguloVoltajes(self.Vbn, "+", 120)
+				if(self.Vcn == None):
+					self.Vcn = self.calcularAnguloVoltajes(self.Vbn, "+", 240)
+			
+			if(self.secuencia == "ACB"):
+				if(self.Van == None):
+					self.Van = self.calcularAnguloVoltajes(self.Vbn, "-", 120)
+				if(self.Vcn == None):
+					self.Vcn = self.calcularAnguloVoltajes(self.Vbn, "-", 240)
+
+
+		if(self.Vcn != None):
+			if(self.secuencia == "ABC"):
+				if(self.Van == None):
+					self.Van = self.calcularAnguloVoltajes(self.Vcn, "-", 120)
+				if(self.Vbn == None):
+					self.Vbn = self.calcularAnguloVoltajes(self.Vcn, "-", 240)
+			
+			if(self.secuencia == "ACB"):
+				if(self.Van == None):
+					self.Van = self.calcularAnguloVoltajes(self.Vcn, "+", 120)
+				if(self.Vbn == None):
+					self.Vbn = self.calcularAnguloVoltajes(self.Vcn, "+", 240)
+
+
+		#Si tenemos algun voltaje de fase calculamos los de lineas
+		if(self.Van != None or self.Vbn != None or self.Vcn != None):
+			if(self.Vab == None):
+			#Vab = Van*sqrt(3)<30
+				self.Vab = self.calcularVLinea(self.Van, self.secuencia)
+			if(self.Vbc == None):
+				#Vbc = Vbn*sqrt(3)<30
+				self.Vbc = self.calcularVLinea(self.Vbn, self.secuencia)
+			if(self.Vca == None):
+				#Vca = Vcn*sqrt(3)<30
+				self.Vca = self.calcularVLinea(self.Vcn, self.secuencia)
 
 	#getters
+	#Retorna el voltaje de linea Vab
 	def getVab(self):
 		return self.Vab
+	#Retorna el voltaje de linea Vbc 
 	def getVbc(self):
 		return self.Vbc
+	#Retorna el voltaje de linea Vca
 	def getVca(self):
 		return self.Vca
+	#Retorna el voltaje de fase Van
 	def getVan(self):
 		return self.Van
+	#Retorna el voltaje de fase Vbn
 	def getVbn(self):
 		return self.Vbn
+	#Retorna el voltaje de fase Vcn
 	def getVcn(self):
 		return self.Vcn
 
+
+class FuenteDelta(Fuente):
+	pass
+
+class FuenteEstrella(Fuente):
+	pass
+
+class CargaMonofasica(object):
+	#Atributos
+	faseUno = None
+	faseDos = None
+	I       = None
+	V       = None
+	Z		= None
+	FP 		= None
+	angulo 	= None
+	P       = None
+	Q  		= None
+	S 		= None
+
+
+class Carga(object):
+
+class CargaDelta(Carga):
+
+class CargaEstrella(Carga):
+
+class Conductor(object):
+	
