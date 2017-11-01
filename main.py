08b41d8ea51e53773f93cf53c5f8d0447cf75aac
Victor Mora <vmora@cipreporting.com>
2017-10-31 20:59:36 -0600

    Agregado un archivo aparte para las funciones, y avances en la GUI

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from functions import *
 import kivy
 kivy.require('1.9.0')
 
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
+from kivy.core.window import Window
 
 #Variables globales#
 fuente = []
@@ -16,20 +18,31 @@ class SelecVab(Popup):
 	pass
 
 class AppLayout(FloatLayout):
-	pass
+	def setVoljateAB(self):
+		selecVab = SelecVab()
+		selecVab.open()
 
+	def crearFuente(self):
+		selecVab = SelecVab()
+		selecVab.open()
+
+	def setTipoFuente(self):
+		selecFuente = SelecFuente()
+		selecFuente.open()
+		selecFuente.bind(on_dismiss=setTipoFuente())
 class mainApp(App):
-
+	
 	def build(self):
 		#selecFuente = SelecFuente()
 		#selecFuente.open()
 		#selecVab = SelecVab()
 		#selecVab.open()
+		#fuenteDelta = FuenteDelta()
+		Window.clearcolor = (.4, .4, .4, 1)
 		return AppLayout()
 
 if __name__ == '__main__':
     mainApp().run()
-		
 
 
 
