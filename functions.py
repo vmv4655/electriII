01013dce55a8f166bb722c05f35c76efb06e8817
Alexfc2015 <alexfc-7-6@hotmail.com>
2017-11-13 14:56:59 -0600

    Todas las funciones estan listas para unir con el GUI

--- functions.py
+++ functions.py
@@ -1,4 +1,49 @@
 import cmath, math
+#FUNCION PARA OBTENER EL FACTOR DE POTENCIA TOTAL
+def FP (LWI,LWII,angulo1,angulo2):
+
+       P      = LWI + LWII
+       LWQ1   = LWI*((math.tan(angulo1*math.pi/180)))
+       LWQ2   = LWII*((math.tan(angulo2*math.pi/180)))
+       Q      = LWQ1+LWQ2
+       angulo = math.atan(Q/P)
+       Fp     = math.cos(angulo)
+       print(Fp)
+       print(P)
+       print(Q)
+
+# FUNCION PARA OBTENER LAS CORRIENTES DE LINEA DE LA FUENTE
+def getCargasLinea(ListaCargas, secuencia):
+        ItrifasicaA = 0
+        ItrifasicaB = 0
+        ItrifasicaC = 0
+        for x in range(0,len(ListaCargas)):
+                carga=ListaCargas[x]
+                if (carga.getTipoCarga()=="Trifasico"):
+                        ItrifasicaA += carga.getIA()
+                        ItrifasicaB += carga.getIB()
+                        ItrifasicaC += carga.getIC()
+                elif (carga.getTipoCarga()=="Monofasico"):
+                        if (carga.getConexion() == "AB" and secuencia== "ABC"):
+                                ItrifasicaA += carga.getIL()
+                        elif (carga.getConexion() == "AB" and secuencia== "ACB"):
+                                ItrifasicaB += carga.getIL()
+                        elif (carga.getConexion() == "BC" and secuencia== "ABC"):
+                                ItrifasicaB += carga.getIL()
+                        elif (carga.getConexion() == "BC" and secuencia== "ACB"):
+                                ItrifasicaC += carga.getIL()                        
+                        elif (carga.getConexion() == "CA" and secuencia== "ABC"):
+                                ItrifasicaC += carga.getIL()
+                        elif (carga.getConexion() == "CA" and secuencia== "ACB"):
+                                ItrifasicaA += carga.getIL()
+                        elif (carga.getConexion() == "AN"):
+                                ItrifasicaA += carga.getIL()
+                        elif (carga.getConexion() == "BN"):
+                                ItrifasicaB += carga.getIL()
+                        elif (carga.getConexion() == "CN"):
+                                ItrifasicaC += carga.getIL()
+        return (ItrifasicaA, ItrifasicaB, ItrifasicaC)
+
 
 #Verifica si un valor es de tipo entero o flotante
 def isFloatOrInt(number):
@@ -284,9 +329,489 @@ class Fuente(object):
 	def getVcn(self):
 		return self.Vcn
 
-class FuenteDelta(Fuente):
-	pass
 
-class FuenteEstrella(Fuente):
-	pass
+#FUNCIONES PARA CARGAS MONOFASICAS(MOTORES MONOFASICOS,CARGAS RESISTIVAS)
 
+#CORRIENTES LINEA PARA CARGAS MONOFASICAS 
+
+class cargaMonofasica(object):
+    tipo="Monofasica"
+    conexion=None
+    Il    = None
+    voltaje=None
+    EaN   = None
+    EbN   = None
+    EcN   = None
+    Eab   = None
+    Ebc   = None
+    Eca   = None
+    cita  = None
+
+    def __init__(self, fuente):
+        self.EaN   = fuente.getVab()
+        self.EbN   = fuente.getVbc()
+        self.EcN   = fuente.getVca()
+        self.Eab   = fuente.getVan()
+        self.Ebc   = fuente.getVbn()
+        self.Eca   = fuente.getVcn()
+    
+    # sele pregunta al usuario el tipo de carga(motores, resistivas) Ademas de la conexion (AB, BC, CN, etc..)
+    def setConexion(self, conexion):
+        if (conexion == "AB"):  # SE ASIGNA UN VOLTAGE SEGUN SELLECCION DE CONECCION#
+                self.voltaje =  self.EaN
+                self.conexion="AB"
+        if (conexion ==  "BC"):
+                self.voltaje =  self.EbN 
+                self.conexion="BC"   
+        if (conexion == "CA"):
+                self.voltaje =  self.EcN
+                self.conexion="CA"
+        if (conexion == "AN"):
+                self.voltaje =  self.Eab  
+                self.conexion="AN"
+        if (conexion == "BN"):
+                self.voltaje = self.Ebc 
+                self.conexion="BN"
+        if (conexion == "CN"):
+                self.voltaje = self.Eca
+                self.conexion="CN"
+    def setTipoCarga(self,  carga):
+        if (carga == "Monofasicas"):
+            self.tipocarga=carga
+        elif (carga=="Motores"):
+            self.tipocarga=carga
+
+
+    def setDatosMonofase(self, P=None, Q=None, S=None, fp=None, direccion=None, Cantidad=None, Z=None):
+        cita=None
+        if (Z != None):
+            self.IL=self.voltaje/Z
+        else:
+            if (direccion== "Adelanto"):
+                cita  = -(math.acos(fp)*180/math.pi)
+            else:
+                if (direccion == "Atraso"):
+                    cita  = (math.acos(fp)*180/math.pi)
+            if (P==None and S==None):
+                P=(a/math.sin(cita*math.pi/180))*math.cos(cita*math.pi/180)
+            if (P==None and S==None):
+                P=(a/math.sin(cita*math.pi/180))*math.cos(cita*math.pi/180)
+            elif (P==None and Q==None):
+                P=S*fp
+            print(getPolFromRec(self.voltaje))
+            si  = (P/fp)*Cantidad
+            sf  = getRecFromPol(1, cita)
+            S   = si*sf
+            Ilp = (S/self.voltaje)
+
+            R   = Ilp.real
+            I   = Ilp.imag
+            self.IL  = complex(R, -I)
+
+    def setDatosMotorMonofase(self, Cantidad, Hp, fp, n, direccion):
+        cita=None
+        if (direccion == "Adelanto"):
+            cita  = -(math.acos(fp)*180/math.pi)
+        else:
+            if (direccion == "Atraso"):
+                cita  = (math.acos(fp)*180/math.pi)
+        Potencia   = Cantidad*Hp*746*1/n
+        Si         = Potencia/fp
+        Sf         = getRecFromPol(1, cita)
+        S          = Si*Sf
+        Ilp        = (S/self.voltaje)
+        R          = Ilp.real
+        I          = Ilp.imag
+        self.IL    = complex(R, -I)   
+            
+    def getIL(self):
+        return self.IL   
+
+
+        
+#FUNCINES PARA CARGAS TRIFASICA,(CONEXCIONES DELTA, ESTRELLA,MOTORES)
+
+class estrellaBal(object):
+
+    tipo="Trifasica"
+    IA=None
+    IB=None
+    IC=None
+
+    def __init__(self, fuente):
+        EAn=fuente.getVan()
+        EBn=fuente.getVbn()
+        ECn=fuente.getVcn()
+        Eab=fuente.getVab()
+        Ebc=fuente.getVbc()
+        Eca=fuente.getVca()
+
+    def corrientesLinea(self,EAn,EBn,ECn,z):
+        self.IA=EAn/z
+        self.IB=EBn/z
+        self.IC=ECn/z
+
+    def getIA(self):
+        return self.IA
+    def getIB(self):
+        return self.IB
+    def getIC(self):
+        return self.IC
+
+    def getTipoCarga(self):
+        return self.tipo
+       
+#PARA CARGAS ESTRELLA DESBALANCEADAS CON NEUTRO# 
+class estrellaDesbal(object):
+
+    tipo="Trifasica"
+    IA=None
+    IB=None
+    IC=None
+    IN=None
+    def __init__(self, fuente):
+        EAn=fuente.getVan()
+        EBn=fuente.getVbn()
+        ECn=fuente.getVcn()
+        Eab=fuente.getVab()
+        Ebc=fuente.getVbc()
+        Eca=fuente.getVca()
+    def corrientesLinea(self,EAn,EBn,ECn,za,zb,zc):
+        self.IA=EAn/za
+        self.IB=EBn/zb
+        self.IC=ECn/zc
+        self.IN=self.IA+self.IB+self.IC
+    def getIA(self):
+        return self.IA
+    def getIB(self):
+        return self.IB
+    def getIC(self):
+        return self.IC 
+    def getIN(self):
+        return self.IN
+
+    def getTipoCarga(self):
+        return self.tipo
+       
+#PARA CARGAS ESTRELLA DESBALANCEADAS SIN NEUTRO CONECTADA A FUENTE ESTRELLA#
+class estrellaDesbalSinNeutro(object):
+
+    tipo="Trifasica"
+    IA=None
+    IB=None
+    IC=None
+    IN=None
+    def __init__(self, fuente):
+
+        EAB=fuente.getVab()
+        EBC=fuente.getVbc()
+        ECA=fuente.getVca()
+    def corrientesLinea(self,EAB,EBC,ECA,za,zb,zc):
+        self.IA=(EAB*zc-ECA*zb)/(za*zb+za*zc+zb*zc)
+        self.IC=(ECA*zb-EBC*za)/(za*zb+za*zc+zb*zc)
+        self.IB=(EBC*za-EAB*zc)/(za*zb+za*zc+zb*zc)
+    def getIA(self):
+        return self.IA
+    def getIB(self):
+        return self.IB
+    def getIC(self):
+        return self.IC 
+
+    def getTipoCarga(self):
+        return self.tipo
+       
+
+
+
+#PARA CARGAS ESTRELLA DESBALANCEADAS CONECTADA A DELTA(SIN NEUTRO)# 
+class estrellaDesbalfdelta(object):
+    tipo="Trifasica"
+    IA=None
+    IB=None
+    IC=None
+    def __init__(self, fuente):
+        EAB=fuente.getVab()
+        EBC=fuente.getVbc()
+        ECA=fuente.getVca()
+    def corrientesLinea(self,EAB,EBC,ECA,za,zb,zc):
+        self.IA=(EAB*zc-ECA*zb)/(za*zb+za*zc+zb*zc)
+        self.IC=(ECA*zb-EBC*za)/(za*zb+za*zc+zb*zc)
+        self.IB=(EBC*za-EAB*zc)/(za*zb+za*zc+zb*zc)
+    def getIA(self):
+        return self.IA
+    def getIB(self):
+        return self.IB
+    def getIC(self):
+        return self.IC 
+
+    def getTipoCarga(self):
+        return self.tipo   
+               
+#PARA CARGAS DELTA BALANCEADAS# 
+class deltaBal(object):
+    tipo="Trifasica"
+    IAB=None
+    IBC=None
+    ICA=None
+    Ia=None
+    Ib=None
+    Ic=None
+    def __init__(self, fuente):
+        EAB=fuente.getVab()
+        EBC=fuente.getVbc()
+        ECA=fuente.getVca()
+ 
+    def corrientes(self,EAB,EBC,ECA,z):
+        self.IAB=EAB/z
+        self.IBC=EBC/z
+        self.ICA=ECA/z
+        self.Ia=self.IAB*(complex(1.5,-0.8660254038))
+        self.Ib=self.IBC*(complex(1.5,-0.8660254038))
+        self.Ic=self.ICA*(complex(1.5,-0.8660254038))
+    def getIAB(self):
+        return self.IAB
+    def getIBC(self):
+        return self.IBC
+    def getICA(self):
+        return self.ICA
+    def getIa(self):
+        return self.Ia
+    def getIb(self):
+        return self.Ib
+    def getIc(self):
+        return self.Ic
+    
+    def getTipoCarga(self):
+        return self.tipo   
+
+#PARA CARGA DELTA DESBALANCEADAS#
+class deltaDesbal(object):
+    tipo="Trifasica"
+    IAB=None
+    IBC=None
+    ICA=None
+    Ia=None
+    Ib=None
+    Ic=None
+    def __init__(self, fuente):
+        EAB=fuente.getVab()
+        EBC=fuente.getVbc()
+        ECA=fuente.getVca()
+    def corrienteLinea(self,EAB,EBC,ECA,za,zb,zc):
+        self.IAB=EAB/za
+        print(EAB)
+        print(za)
+        print(self.IAB)
+        self.IBC=EBC/zb
+        self.ICA=ECA/zc
+        self.Ia=self.IAB-self.ICA
+        self.Ib=self.IBC-self.IAB
+        self.Ic=self.ICA-self.IBC
+    def getIAB(self):
+        return self.IAB
+    def getIBC(self):
+        return self.IBC
+    def getICA(self):
+        return self.ICA
+    def getIa(self):
+        return self.Ia
+    def getIb(self):
+        return self.Ib
+    def getIc(self):
+        return self.Ic
+
+    def getTipoCarga(self):
+        return self.tipo   
+#Para motores,Apartir de Potencia,cantidad,Hp,FP,D,Eficiencia
+#D=direccion(1=adelanto,0=atraso)
+class cargaMotor(object):
+    tipo="Trifasica"
+    IA=None
+    IB=None
+    Ic=None
+    def __init__(self, fuente):
+        EAn=fuente.getVan()
+        EBn=fuente.getVbn()
+        ECn=fuente.getVcn()
+    def polarTorectangular(v,ang):
+        x=v*(math.cos(ang*math.pi/180))
+        y=v*(math.sin(ang*math.pi/180))
+        com=complex(x,y)
+        return(com)
+    def Il_motor(self,EAn,potencia,cantidad,Hp,fp,n): 
+        cita=(math.degrees(math.acos(fp)))
+        PIII=cantidad*Hp*746*potencia*(1/n)
+        PI=PIII/3
+        si  = (PI/fp)
+        sf  = getRecFromPol(1, cita)
+        S   = si*sf
+        Ilp = (S/EAn)
+        R   = Ilp.real
+        I   = Ilp.imag
+        self.IA  = complex(R, -I)
+        self.IB=self.IA*(complex(-0.5,-0.8660254038))
+        self.IC=self.IA*(complex(-0.5,+0.8660254038))
+    def getIA(self):
+        return self.IA
+    def getIB(self):
+        return self.IB
+    def getIC(self):
+        return self.IC
+
+    def getTipoCarga(self):
+        return self.tipo   
+        
+#FUNCIONES PARA CALCULAR LA POTENCIA ACTIVA Y REACTIVA CON METODO DE 2 Y 3 VATIMETROS
+
+
+#MEDICION DE POTENCIA ACTIVA#
+##METODO DE 2 VATIMETROS 
+class dosVatimetrosactiva(object):
+    Eab   = None
+    Ebc  = None
+    Eca   = None
+    lectura=None
+    def __init__(self, fuente):
+        Eab=fuente.getVab()
+        Ebc=fuente.getVbc()
+        Eca=fuente.getVca()
+    def dosVatimetrosE(self, Eab, Ebc, Eca, IAl, IBl, ICl, C):#C=punto comun
+        LWI=0
+        LWII=0
+        if (self.C == str("A")):
+            self.Vab, self.angA  = getPolFromRec(Eab)
+            self.Vca, self.angB  = getPolFromRec(Eca)
+            self.IB, self.angC   = getPolFromRec(IBl)
+            sel.IC, self.angD    = getPolFromRec(ICl)
+            
+            self.LWI  = Vab*IB*math.cos((ang1+180-angC)*math.pi/180)
+            self.LWII = vca*IC*math.cos((ang2-angD)*math.pi/180) 
+
+        elif (self.C == str("B")):
+            self.Vab, self.angA = getPolFromRec(Eab)
+            self.Vbc, self.angB = getPolFromRec(Ebc)
+            self.IA, self.angC  = getPolFromRec(IAl)
+            self.IC, self.angD  = getPolFromRec(ICl)
+            
+            self.LWI  = vab*IA*math.cos((angA-angC)*math.pi/180)
+            self.LWII = vbc*IC*math.cos((angB+180-angD)*math.pi/180)
+
+        elif (self.C == str("C")):
+            self.Vca, self.angA = getPolFromRec(Eca)
+            self.Vbc, self.angB = getPolFromRec(Ebc)
+            self.IA, self.angC  = getPolFromRec(IAl)
+            self.IB, self.angD  = getPolFromRec(IBl)
+
+            self.LWI  = Vca*IA*math.cos((angA+180-angC)*math.pi/180)
+            self.LWII = Vbc*IB*math.cos((angB-angD)*math.pi/180)
+
+        lectura=LWI+LWII
+    def getPotenciaActiva(self):
+        return self.lectura
+       
+#METODO DE 3 VATIMETROS POTENCIA ACTIVA#
+class tresVatimetrosactiva(object):
+    Lectura = None
+    Ean=None
+    Ebn=None
+    Ecn=None
+    def __init__(self, fuente):
+        self.Eab=fuente.getVan()
+        self.Ebn=fuente.getVbn()
+        self.Ecn=fuente.getVcn()
+
+    
+    def tres_vatimetrosE(self, IA, IB, IC, Ean, Ebn, Ecn):
+        self.EaB, self.angA = getPolFromRec(Ean)
+        self.EbC, self.angB = getPolFromRec(Ebn)
+        self.EcA, self.angC = getPolFromRec(Ecn)
+
+        self.IA, self.angD  = getPolFromRec(IAl)
+        self.IB, self.angE  = getPolFromRec(IBl)
+        self.IC, self.angF  = getPolFromRec(ICl)
+
+        self.LWI      = EaB*IA*cos((angA-angD)*math.pi/180)
+        self.LWII     = EbC*IB*cos((angB-angE)*math.pi/180)
+        self.LWIII    = EcA*IC*cos((angC-angF)*math.pi/180)
+        self.lectura  = LWI+LWII+LWIII
+
+    def getPotenciaActiva(self):
+        return self.lectura
+
+
+#MEDICION DE POTENCIA RECTIVA#
+##METODO DE 2 VATIMETROS 
+class dosVatimetrosreactiva(object):
+    Eab   = None
+    Ebc   = None
+    Eca   = None
+    lectura=None
+    def __init__(self, fuente):
+        Eab=fuente.getVab()
+        Ebc=fuente.getVbc()
+        Eca=fuente.getVca()
+   
+    def dos_vatimetrosE(self,IAl, IBl, ICl, Eab, Ebc, Eca,C):#C=punto comun
+        LWI=0
+        LWII=0
+        if (self.C == str("A")):
+            self.Vab, self.angA  = getPolFromRec(Eab)
+            self.Vca, self.angB  = getPolFromRec(Eca)
+            self.IB, self.angC   = getPolFromRec(IBl)
+            sel.IC, self.angD    = getPolFromRec(ICl)
+            
+            self.LWI  = Vab*IB*math.sin((ang1+180-angC)*math.pi/180)
+            self.LWII = vca*IC*math.sin((ang2-angD)*math.pi/180) 
+
+        elif (self.C == str("B")):
+            self.Vab, self.angA = getPolFromRec(Eab)
+            self.Vbc, self.angB = getPolFromRec(Ebc)
+            self.IA, self.angC  = getPolFromRec(IAl)
+            self.IC, self.angD  = getPolFromRec(ICl)
+            
+            self.LWI  = vab*IA*math.sin((angA-angC)*math.pi/180)
+            self.LWII = vbc*IC*math.sin((angB+180-angD)*math.pi/180)
+
+        elif (self.C == str("C")):
+            self.Vca, self.angA = getPolFromRec(Eca)
+            self.Vbc, self.angB = getPolFromRec(Ebc)
+            self.IA, self.angC  = getPolFromRec(IAl)
+            self.IB, self.angD  = getPolFromRec(IBl)
+
+            self.LWI  = Vca*IA*math.sin((angA+180-angC)*math.pi/180)
+            self.LWII = Vbc*IB*math.sin((angB-angD)*math.pi/180)
+
+        lecctura=LWI+LWII
+
+    def getPotenciaReActiva(self):
+        return self.lectura
+        
+#METODO DE 3 VATIMETROS POTENCIA ACTIVA#
+class tresVatimetrosreactiva(object):
+    Lectura = None
+    Ean = None
+    Ebn = None
+    Ecn = None
+    Lectura = None
+    def __init__(self, fuente):
+        Ean=fuente.getVan()
+        Ebn=fuente.getVbn()
+        Ecn=fuente.getVca()
+  
+    def tres_vatimetrosE(self, IAl, IBl, ICl, Ean, Ebn, Ecn):
+        self.EaB, self.angA = getPolFromRec(Ean)
+        self.EbC, self.angB = getPolFromRec(Ebn)
+        self.EcA, self.angC = getPolFromRec(Ecn)
+
+        self.IA, self.angD  = getPolFromRec(IAl)
+        self.IB, self.angE  = getPolFromRec(IBl)
+        self.IC, self.angF  = getPolFromRec(ICl)
+
+        self.LWI      = EaB*IA*sin((angA-angD)*math.pi/180)
+        self.LWII     = EbC*IB*sin((angB-angE)*math.pi/180)
+        self.LWIII    = EcA*IC*sin((angC-angF)*math.pi/180)
+        self.lectura  = LWI+LWII+LWIII
+
+    def getPotenciaReActiva(self):
+        return self.lectura
+        
